2c44512d277b4e770eb2c88062f6157830641fb6
AngelMn2005 <bastidasangel192005@gmail.com>
2026-05-19 19:53:18 -0500

    Delete tarea directory

--- tarea/tarea/nivel_avanzado/__init__.py
+++
@@ -1,9 +0,0 @@
-from .BLOQUE_12 import Bloque_12
-from .BLOQUE_13 import Bloque_13
-from .BLOQUE_14 import Bloque_14
-from .BLOQUE_15 import Bloque_15
-from .BLOQUE_16 import Bloque_16
-from .BLOQUE_17 import Bloque_17
-
-
-__all__ = ["Bloque_12","Bloque_13" ,"Bloque_14","Bloque_15","Bloque_16","Bloque_17"]
--- tarea/tarea/views/__init__.py
+++
@@ -1,3 +0,0 @@
-from .app import MenuPrincipal
-
-__all__ = ["MenuPrincipal"]
